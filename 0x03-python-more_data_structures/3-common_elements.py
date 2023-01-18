8ea2699418477f4480f5b3eafef8010d748513b8
jbees918 <5590@holbertonstudents.com>
2023-01-18 03:47:31 +0000

    threefix

--- 0x03-python-more_data_structures/3-common_elements.py
+++ 0x03-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
-
-n_elements(set_1, set_2):
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
     # This function takes in two sets as parameters and returns
     # a list of common elements present in both sets
     # Using list comprehension to iterate through all elements
