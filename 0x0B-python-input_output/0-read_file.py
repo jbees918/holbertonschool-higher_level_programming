4358a49e2965a6346bb3ace341deb0ca11027f3e
jbees918 <5590@holbertonstudents.com>
2023-02-01 03:10:20 +0000

    fixoneandten

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-""" Reads text file (UTF8) then prints to stdout """
+eads text file (UTF8) and prints it to stdout """
+
 
 def read_file(filename=""):
-	""" Reads and prints to stdout """
-	with open(filename, encoding="utf-8" as my_file:
-		print(my_file.read(), end="")
+    """ Reads and prints to stdout """
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read(), end="")
--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" Write class student that defines
-a student by: (based on 9-student.py) ""
+""" Write a class Student that defines
+a student by: (based on 9-student.py) """
 
 
-class student:
-	""" Student class """
-	def __init__(self, first_name, last_name, age):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-	def to_json(self, attrs=None):
-		""" Convert student object to json """
-		if attrs is None:
-			return vars(self)
-		new_dict = {}
-		for i in attrs:
-			try:
-				new_dict[i] = vars(self)[i]
-			except Exception:
-				pass
-		return new_dict
+    def to_json(self, attrs=None):
+        """Convert a Student object to JSON"""
+        if attrs is None:
+            return vars(self)
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = vars(self)[i]
+            except Exception:
+                pass
+        return new_dict
