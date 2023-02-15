5a4c755fc4c0c449eec82cf0366cb93f842ea808
jbees918 <5590@holbertonstudents.com>
2023-02-14 18:59:37 -0800

    program implementation

---
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- import database from 'hbtn_0d_tvshows'
+-- Lists all shows in 'hbtn_0d_tvshows' with at least
+-- one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
