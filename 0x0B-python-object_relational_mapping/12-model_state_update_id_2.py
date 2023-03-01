319f7f3f07d6cee32ab9b9f56930d6e8d052de0d
jbees918 <5590@holbertonstudents.com>
2023-02-28 21:21:02 -0600

    model additions

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,3 +3,13 @@
     Script that changes the name of a 
     State object in the database
 """
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main_":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.
